d43ba4e2c8ced013c249d6c999b5013fb46fbb27
Akpan Godswill <55089434+GeeAkpan@users.noreply.github.com>
2020-07-17 12:43:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -152,19 +152,19 @@ if (isset($JoinList)) {
                 </p>
             </div>
 
-            <div class="col-lg-6 ml-auto text-center">
+            <div class="col-lg-6 ml-auto text-center animate__animated animate__bounce animate_delay-2s">
                 <img src="img/Pixel 3 XL (6) 1.png" class="img-fluid">
             </div>
         </div>
 
         <div class="row ml-auto">
-            <div class="col-lg-4 ml-auto text-center">
+            <div class="col-lg-4 ml-auto text-center animate__animated animate__fadeInDownBig animate_delay-2s">
                 <img src="img/Pixel 3 XL (7) 2.png" class="img-fluid">
             </div>
-            <div class="col-lg-4 ml-auto text-center">
+            <div class="col-lg-4 ml-auto text-center animate__animated animate__fadeInRightBig animate_delay-2s">
                 <img src="img/Pixel 3 XL (8) 1.png" class="img-fluid">
             </div>
-            <div class="col-lg-4 ml-auto text-center">
+            <div class="col-lg-4 ml-auto text-center animate__animated animate__fadeInDownBig animate_delay-2s">
                 <img src="img/Pixel 3 XL (8) 2.png" class="img-fluid">
             </div>
         </div>
@@ -183,6 +183,10 @@ if (isset($JoinList)) {
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+     </script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
